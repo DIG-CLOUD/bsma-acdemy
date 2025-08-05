6677cfd6da811312f634012ce39ec27111c1da24
M.Wasefy <mohamed.alwasefy@vitacareonline.com>
2025-08-05 17:25:57 +0300

    Bsma Edu Website New Module

---
+++ bsma_website/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'BSMA Website',
+    'version': '17.0.1.0.0',
+    'category': 'Website',
+    'summary': 'Custom website module for BSMA education platform',
+    'description': """
+        This module customizes the website slides module for BSMA education platform.
+        It replaces the courses_all template with a responsive Arabic design.
+    """,
+    'author': 'BSMA',
+    'website': '',
+    'depends': ['website_slides', 'website'],
+    'data': [
+        'views/templates.xml',
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            'bsma_website/static/src/css/bsma_website.css',
+        ],
+    },
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
